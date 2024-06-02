ad167f9c67b354ed41099cd6195b7e0f51af65fe
Sergey Lihobabin <sergey.lihobabin@gmail.com>
2024-06-02 14:09:32 +0500

    basic logging to file added

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 import json
 import os
-import random
 
 from aiohttp import web
+import asyncio
 import serial
 from serial.serialutil import SerialException
-import asyncio
 
+from telemetry import TelemetryRecord, record_from_serial, record_from_random
 from page import PAGE_TEMPLATE
 
 PORT = int(os.environ.get('PORT', 8080))
@@ -20,25 +19,6 @@ SERIAL_TIMEOUT = 0.1
 SERIAL_WAIT_TIME = 0.05
 SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_DIRECTORY = os.path.join(SOURCE_DIR, 'logs')
-
-@dataclass
-class TelemetryRecord:
-    amper_hours: float
-    voltage: float
-    current: float
-    speed: float
-    distance: float
-    motor_temp: float
-    rpm: float
-    human_watts: float
-    human_torque: float
-    throttle_input: float
-    throttle_output: float
-    aux_a: float
-    aux_d: float
-    mode: int
-    flags: str
-    is_brake_pressed: bool
 
 
 async def http_handler(request: web.Request):
@@ -57,65 +37,6 @@ async def websocket_handler(request: web.Request):
         request.app['websockets'].remove(ws)
     return ws
 
-def record_from_serial(ser: serial.Serial) -> TelemetryRecord | None:
-    line = ser.readline()
-    values = line.decode("utf-8").split("\t")
-    if len(values) < 14:
-        return None
-    flags = values[13].replace('\r\n', '')
-    return TelemetryRecord(
-        amper_hours=float(values[0]),
-        voltage=float(values[1]),
-        current=float(values[2]),
-        speed=float(values[3]),
-        distance=float(values[4]),
-        motor_temp=float(values[5]),
-        rpm=float(values[6]),
-        human_watts=float(values[7]),
-        human_torque=float(values[8]),
-        throttle_input=float(values[9]),
-        throttle_output=float(values[10]),
-        aux_a=float(values[11]),
-        aux_d=float(values[12]),
-        flags=flags,
-        mode=int(flags[0]),
-        is_brake_pressed='B' in flags
-    )
-
-def get_random_value(from_: float, to_: float, step:float, previous: float | None) -> float:
-    if previous is not None:
-        if random.uniform(0, 10) < 1:
-            result = previous + random.uniform(-step, step)
-            if result < from_:
-                result = from_
-            elif result > to_:
-                result = to_
-        else:
-            result = previous
-    else:
-        result = random.uniform(from_, to_)
-    return round(result, 2)
-
-def record_from_random(previous: TelemetryRecord | None) -> TelemetryRecord:
-    return TelemetryRecord(
-        amper_hours=get_random_value(0, 10, 0.01, previous and previous.amper_hours),
-        voltage=get_random_value(35, 55, 0.1, previous and previous.voltage),
-        current=get_random_value(0, 25, 1, previous and previous.current),
-        speed=get_random_value(0, 50, 1, previous and previous.speed),
-        distance=get_random_value(0, 100, 0.1, previous and previous.distance),
-        motor_temp=get_random_value(20, 80, 0.1, previous and previous.motor_temp),
-        rpm=get_random_value(0, 200, 10, previous and previous.rpm),
-        human_watts=get_random_value(0, 500, 1, previous and previous.human_watts),
-        human_torque=get_random_value(0, 50, 0.1, previous and previous.human_torque),
-        throttle_input=get_random_value(0, 100, 0.1, previous and previous.throttle_input),
-        throttle_output=get_random_value(0, 100, 0.1, previous and previous.throttle_output),
-        aux_a=0,
-        aux_d=0,
-        flags='',
-        mode=1,
-        is_brake_pressed=False
- 
-    )
 
 async def send_telemetry(app: web.Application, telemetry: TelemetryRecord  | None):
     if telemetry is not None:
@@ -123,27 +44,36 @@ async def send_telemetry(app: web.Application, telemetry: TelemetryRecord  | Non
         for ws in app['websockets']:
             await ws.send_str(data)
 
+def write_to_log(app: web.Application, telemetry: TelemetryRecord | None):
+    if telemetry is not None:
+        app['log_file'].write(f'{datetime.now().isoformat()} {telemetry.__dict__}\n')
+
 async def read_telemetry(app: web.Application):
     try:
         with serial.Serial(INTERFACE, 9600, timeout=SERIAL_TIMEOUT) as ser:
             while True:
                 telemetry = record_from_serial(ser)
+                write_to_log(app, telemetry)
                 await send_telemetry(app, telemetry)
                 await asyncio.sleep(SERIAL_WAIT_TIME)
     except SerialException:
         print(f'Could not open serial interface {INTERFACE}')
 
+
 async def read_random_telemetry(app: web.Application):
     previous = None
     while True:
         telemetry = record_from_random(previous)
-        await send_telemetry(app, telemetry)
         previous = telemetry
+        write_to_log(app, telemetry)
+        await send_telemetry(app, telemetry)
         await asyncio.sleep(SERIAL_WAIT_TIME)
+
 
 async def on_shutdown(app: web.Application):
     for ws in app['websockets']:
         await ws.close(code=999, message='Server shutdown')
+
 
 async def start_background_tasks(app: web.Application):
     app[TELEMETRY_TASK] = asyncio.create_task(read_random_telemetry(app))
@@ -160,7 +90,9 @@ def init():
     app = web.Application()
     app['websockets'] = []
     Path(LOG_DIRECTORY).mkdir(parents=True, exist_ok=True)
-    app['log_file'] = open(os.path.join(LOG_DIRECTORY, f'{datetime.now().isoformat()}.log'), 'w+')
+    log_file = os.path.join(LOG_DIRECTORY, f'{datetime.now().isoformat()}.log')
+    print(f'Logging to {log_file}')
+    app['log_file'] = open(log_file, 'w+')
     app.add_routes([
         web.get('/',   http_handler),
         web.get('/ws', websocket_handler),
@@ -169,6 +101,7 @@ def init():
     app.on_cleanup.append(cleanup_background_tasks)
     app.on_shutdown.append(on_shutdown)
     return app
+
 
 def start_server():
     app = init()
---
+++ telemetry.py
@@ -0,0 +1,85 @@
+from dataclasses import dataclass
+import random
+import serial
+
+
+@dataclass
+class TelemetryRecord:
+    amper_hours: float
+    voltage: float
+    current: float
+    speed: float
+    distance: float
+    motor_temp: float
+    rpm: float
+    human_watts: float
+    human_torque: float
+    throttle_input: float
+    throttle_output: float
+    aux_a: float
+    aux_d: float
+    mode: int
+    flags: str
+    is_brake_pressed: bool
+
+
+def record_from_serial(ser: serial.Serial) -> TelemetryRecord | None:
+    line = ser.readline()
+    values = line.decode("utf-8").split("\t")
+    if len(values) < 14:
+        return None
+    flags = values[13].replace('\r\n', '')
+    return TelemetryRecord(
+        amper_hours=float(values[0]),
+        voltage=float(values[1]),
+        current=float(values[2]),
+        speed=float(values[3]),
+        distance=float(values[4]),
+        motor_temp=float(values[5]),
+        rpm=float(values[6]),
+        human_watts=float(values[7]),
+        human_torque=float(values[8]),
+        throttle_input=float(values[9]),
+        throttle_output=float(values[10]),
+        aux_a=float(values[11]),
+        aux_d=float(values[12]),
+        flags=flags,
+        mode=int(flags[0]),
+        is_brake_pressed='B' in flags
+    )
+
+def get_random_value(from_: float, to_: float, step:float, previous: float | None) -> float:
+    if previous is not None:
+        if random.uniform(0, 10) < 1:
+            result = previous + random.uniform(-step, step)
+            if result < from_:
+                result = from_
+            elif result > to_:
+                result = to_
+        else:
+            result = previous
+    else:
+        result = random.uniform(from_, to_)
+    return round(result, 2)
+
+def record_from_random(previous: TelemetryRecord | None) -> TelemetryRecord:
+    return TelemetryRecord(
+        amper_hours=get_random_value(0, 10, 0.01, previous and previous.amper_hours),
+        voltage=get_random_value(35, 55, 0.1, previous and previous.voltage),
+        current=get_random_value(0, 25, 1, previous and previous.current),
+        speed=get_random_value(0, 50, 1, previous and previous.speed),
+        distance=get_random_value(0, 100, 0.1, previous and previous.distance),
+        motor_temp=get_random_value(20, 80, 0.1, previous and previous.motor_temp),
+        rpm=get_random_value(0, 200, 10, previous and previous.rpm),
+        human_watts=get_random_value(0, 500, 1, previous and previous.human_watts),
+        human_torque=get_random_value(0, 50, 0.1, previous and previous.human_torque),
+        throttle_input=get_random_value(0, 100, 0.1, previous and previous.throttle_input),
+        throttle_output=get_random_value(0, 100, 0.1, previous and previous.throttle_output),
+        aux_a=0,
+        aux_d=0,
+        flags='',
+        mode=1,
+        is_brake_pressed=False
+ 
+    )
+

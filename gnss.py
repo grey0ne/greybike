79f3434aec5de05c4e423bdc945b1c8b68eacbfb
Sergey Lihobabin <sergey.lihobabin@gmail.com>
2024-07-31 22:26:04 +0500

    gnss try once more

--- gnss.py
+++ gnss.py
@@ -15,12 +15,18 @@ GNSS_SERIAL = serial.Serial(
     timeout=1
 )
 
+GLL = 'GLL' # Geographic Position - Latitude/Longitude
+GSV = 'GSV' # GNSS Satellites in View
+GSA = 'GSA' # GNSS DOP and Active Satellites
+
 def gnss_from_serial(ser: serial.Serial) -> GNSSRecord | None:
     logger = logging.getLogger('greybike')
     try:
         line = ser.readline()
         result = NMEAReader.parse(line)
         print(result)
+        if result.msgID == GLL:
+            print(result.lat, result.lon)
     except serial.SerialException as e:
         logger.error('Device error: {}'.format(e))
         return None

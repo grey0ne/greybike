eb974f5b75f567e828b498128175ab8839a8c4bc
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2024-08-01 17:33:48 +0500

    Some experiments with ads

---
+++ ads.py
@@ -0,0 +1,14 @@
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c)
+ads.gain = 1
+chan = AnalogIn(ads, ADS.P0)
+
+for x in range(100):
+    print(f"MQ-135 Voltage: {chan.voltage}V")
+    time.sleep(1)

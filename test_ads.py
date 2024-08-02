673631961059e9d00458ef8e5d7efc0072d89bb7
Sergey Lihobabin <sergey.lihobabin@gmail.com>
2024-08-02 15:13:03 +0500

    ads test script added

---
+++ test_ads.py
@@ -0,0 +1,9 @@
+import time
+from ads import electric_record_from_ads, get_ads_interface
+
+ads = get_ads_interface()
+
+while True:
+    if ads is not None:
+        electric_record_from_ads(ads)
+    time.sleep(1)

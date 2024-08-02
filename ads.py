1b2ca7cbb860600f0226dd7b1cc90b72ee0e12db
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2024-08-02 16:21:16 +0500

    constants tweaked a bit

--- ads.py
+++ ads.py
@@ -6,18 +6,21 @@ from adafruit_ads1x15.analog_in import AnalogIn
 from utils import ElectricalRecord, get_random_value
 
 
-BASE_VOLTAGE = 2.515 # ACS712 20A sensor has 2.5V output when no current is flowing
+BASE_VOLTAGE = 2.5155 # ACS712 20A sensor has 2.5V output when no current is flowing
 AMP_CONVERSION_CF = 9.93 # ACS712 20A coefficient should be exactly 10 (100mV/A) but after calibration this value works better
 
-VOLTAGE_DIVIDER_CF = 21 # 200kOhm / 10kOhm voltage divider
+VOLTAGE_DIVIDER_CF = 21.7 # 200kOhm / 10kOhm voltage divider. And some further calubration
 
+
+def rd(value: float):
+    return round(value, 2)
 
 def electric_record_from_ads(ads: ADS.ADS1115) -> ElectricalRecord:
     current_channel = AnalogIn(ads, ADS.P0) # ACS712 20A sensor connected to A0. Measures current flowing to the bike's electronics
     voltage_channel = AnalogIn(ads, ADS.P1) # Voltage divider connected to A1. Measures battery voltage
     amps = (BASE_VOLTAGE - current_channel.voltage) * AMP_CONVERSION_CF
     battery_voltage = voltage_channel.voltage * VOLTAGE_DIVIDER_CF
-    print(f"A1 Value: {voltage_channel.voltage} Voltage: {battery_voltage} Amps: {amps} Watts: {amps * battery_voltage}")
+    print(f"A1 Value: {rd(voltage_channel.voltage)} Voltage: {rd(battery_voltage)} Amps: {rd(amps)} Watts: {rd(amps * battery_voltage)}")
     return ElectricalRecord(
         timestamp=datetime.timestamp(datetime.now()),
         current=amps,

0c3573a0acc134ff7a11c549701c051eecb18859
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2024-08-04 14:50:28 +0500

    improved gnss parser a bit

--- gnss.py
+++ gnss.py
@@ -16,45 +16,57 @@ GNSS_SERIAL = serial.Serial(
 
 KNOTS_TO_KMH = 1.852
 
-GLL = 'GPGLL' # Geographic Position - Latitude/Longitude
-GSV = 'GSV' # GNSS Satellites in View
-GSA = 'GSA' # GNSS DOP and Active Satellites
-GGA = 'GPGGA' # Global positioning system fix data
-RMC = 'GPRMC' # Recommended Minimum data
-VTG = 'VTG' # Course over ground and Groundspeed
+GLL = ['GPGLL', 'GNGLL'] # Geographic Position - Latitude/Longitude
+GSV = ['GPGSV', 'GLGSV', 'GBGSV', 'GAGSV', 'GQGSV'] # GNSS Satellites in View
+GSA = ['GNGSA'] # GNSS DOP and Active Satellites
+GGA = ['GPGGA', 'GNGGA'] # Global positioning system fix data
+RMC = ['GNRMC','GPRMC'] # Recommended Minimum data
+VTG = ['GNVTG'] # Course over ground and Groundspeed
+
 
 def parse_GGA(values: list[str]) -> GNSSRecord:
-    return GNSSRecord(
-        latitude=float(values[2]),
-        longitude=float(values[4]),
-        sat_num=int(values[7]),
-        hdop=float(values[8]),
-        altitude=float(values[9])
-    )
+    try:
+        return GNSSRecord(
+            latitude=float(values[2]),
+            longitude=float(values[4]),
+            sat_num=int(values[7]),
+            hdop=float(values[8]),
+            altitude=float(values[9])
+        )
+    except ValueError:
+        print(values)
 
 def parse_RMC(values: list[str]) -> GNSSRecord:
-    return GNSSRecord(
-        latitude=float(values[3]),
-        longitude=float(values[5]),
-        speed=float(values[7]) * KNOTS_TO_KMH
-    )
+    try:
+        return GNSSRecord(
+            latitude=float(values[3]),
+            longitude=float(values[5]),
+            speed=float(values[7]) * KNOTS_TO_KMH
+        )
+    except ValueError:
+        print(values)
 
 def parse_GLL(values: list[str]) -> GNSSRecord:
-    return GNSSRecord(
-        latitude=float(values[1]),
-        longitude=float(values[3]),
-    )
+    try:
+        return GNSSRecord(
+            latitude=float(values[1]),
+            longitude=float(values[3]),
+        )
+    except ValueError:
+        print(values)
 
 def process_nmea_line(line: bytes) -> GNSSRecord | None:
     logger = logging.getLogger('greybike')
     nmea_values = line.decode("utf-8").replace('\r\n', '').split(',')
     msgID = nmea_values[0].replace('$', '') 
-    if msgID == GGA:
+    if msgID in GGA:
         return parse_GGA(nmea_values)
-    if msgID == RMC:
+    if msgID in RMC:
         return parse_RMC(nmea_values)
-    if msgID == GLL:
+    if msgID in GLL:
         return parse_GLL(nmea_values)
+    if msgID in GSA + GSV + VTG:
+        return None
     logger.error(f'Unknown message {msgID}')
     return None
 

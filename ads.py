3265087f314d8e88dc818d3cec93b69cb2600eea
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2024-08-09 17:07:25 +0500

    thermistor logic implemented

--- ads.py
+++ ads.py
@@ -8,18 +8,34 @@ from datetime import datetime
 from adafruit_ads1x15.analog_in import AnalogIn
 from utils import ElectricalRecord, get_random_value
 import logging
+import math
 
 
 AMP_CONVERSION_CF = 0.185 # ACS712 5A coefficient (185mv/A)
 
 VOLTAGE_DIVIDER_CF = 21.7 # 200kOhm / 10kOhm voltage divider. And some further calubration
 
+NOMINAL_TEMP = 25
+THERMISTOR_CF = 3950
+THERMISTOR_R = 10000 # 10kOm thermistor
+RESISTOR = 10000 # 10kOm resistor
+
+TEMP_CF = 273.15
+
+def calculate_temp_from_voltage_diff(vcf: float) -> float:
+    thermistor_resistance = (vcf * RESISTOR) / (1 - vcf)
+    temp = 1 / (math.log(thermistor_resistance/THERMISTOR_R) / THERMISTOR_CF + 1 / (NOMINAL_TEMP + TEMP_CF)) - TEMP_CF
+    return temp
 
 
 def electric_record_from_ads(ads: ADS.ADS1115) -> ElectricalRecord:
     current_channel = AnalogIn(ads, ADS.P0) # ACS712 20A sensor connected to A0. Measures current flowing to the bike's electronics
     voltage_channel = AnalogIn(ads, ADS.P1) # Voltage divider connected to A1. Measures battery voltage
     reference_voltage_channel = AnalogIn(ads, ADS.P2) # Voltage divider connected to A2. Should have halved main voltage
+    thermistor_channel = AnalogIn(ads, ADS.P3) # 10K Thermistor with 10K resistor
+    vcf = thermistor_channel.voltage / (reference_voltage_channel.voltage * 2)
+    temp = calculate_temp_from_voltage_diff(vcf)
+    print("Temp: ", temp)
     base_voltage = reference_voltage_channel.voltage
     amps = (base_voltage - current_channel.voltage) / AMP_CONVERSION_CF
     battery_voltage = voltage_channel.voltage * VOLTAGE_DIVIDER_CF

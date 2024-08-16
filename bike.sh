b42aa736e98a5de49e64cbad5af44d9cec170013
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2024-08-16 13:28:59 +0500

    deploy script updated

--- bike.sh
+++ bike.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-
-
-/home/greyone/greybike/.venv/bin/python /home/greyone/greybike/cli.py $1
+if [ $1 = "update" ]; then
+    cd /home/greyone/greybike/
+    git pull
+    cd /home/greyone/greybike/spa
+    npm run build
+    sudo systemctl restart greybike.service
+else
+    /home/greyone/greybike/.venv/bin/python /home/greyone/greybike/cli.py $1
+fi

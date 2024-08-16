bf85985ddfce17575d03c0830009a0b0ae9a53df
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2024-08-16 18:29:14 +0500

    added restart command to control script

--- bike.sh
+++ bike.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-if [ $1 = "update" ]; then
+if [ -z "$1" ]; then
+    echo "Add command"
+elif [ $1 = "update" ]; then
     cd /home/greyone/greybike/
     git pull
     cd /home/greyone/greybike/spa
     npm run build
+    sudo systemctl restart greybike.service
+elif [ $1 = "restart" ]; then
     sudo systemctl restart greybike.service
 else
     /home/greyone/greybike/.venv/bin/python /home/greyone/greybike/cli.py $1

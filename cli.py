000bc245c5780424322afd822c92cd619e5c3de6
Sergey Lihobabin <sergey.lihobabin@gmail.com>
2024-08-28 22:00:59 +0500

    ina228 experiments

--- cli.py
+++ cli.py
@@ -4,11 +4,13 @@ from commands.test_gnss import test_gnss_sensor
 from commands.test_log_agregation import test_calculate_log_agregates
 from commands.test_software_serial import test_software_serial
 from commands.test_ca import test_ca_telemetry
+from commands.test_ina228 import test_ina228
 
 COMMANDS = {
     'test_ads': test_ads_sensor,
     'test_gnss': test_gnss_sensor,
     'test_ca': test_ca_telemetry,
+    'test_ina228': test_ina228,
     'test_log_agregation': test_calculate_log_agregates,
     'test_software_serial': test_software_serial
 }
